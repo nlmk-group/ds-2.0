27dabc1c5bd89870c67cbcd6e483f018692704ce
nlmk-group <120450602+nlmk-group@users.noreply.github.com>
2024-01-18 17:45:06 +0000

    Deploying to gh-pages from  @ 98998563bcc58d13eaaf862350cc13e969f03e2d 🚀

---
+++ assets/Accordion.stories-4dacedfd.js
@@ -0,0 +1,118 @@
+import{M as f,U as g}from"./chunk-HLWAVYOI-653a1315.js";import{s as x,i as y,v as L,A as l}from"./index-a4c35820.js";import{a as n,j as t,F as u}from"./clsx.m-3764cf42.js";import{r as b}from"./index-f1f749bf.js";import{s as a,H as v,E as e,T as h}from"./Tests-27e47391.js";import{F as j}from"./FigmaEmbed-3478e7e6.js";import{P as w}from"./Properties-72516426.js";/* empty css               */import{a as r}from"./index-28aa8d3c.js";import"./index-63601013.js";import"./index-e99519f1.js";import"./index-cd305b84.js";import"./index-147655d7.js";import{T as M}from"./index-fb030d50.js";import"./index-ed56af28.js";import"./index-5196d16d.js";import"./index-2cdebf62.js";import"./index-28363b14.js";import"./index-8c6efd17.js";import{a as C}from"./argsTypes-87b49d50.js";import{u as E}from"./index-4fb8b842.js";import"./iframe-697674bb.js";import"../sb-preview/runtime.js";import"./react-16-8c769108.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-f8a8f14b.js";import"./get-78cac09e.js";import"./callBound-57fbc82c.js";import"./mapValues-f98d729d.js";import"./index-b75c9059.js";import"./uniq-b8c8c302.js";import"./index-356e4a49.js";import"./index-c4c1b386.js";import"./index-29c055d1.js";import"./.jest-test-results-7c3b4ed9.js";import"./IconArticle24-414bfa4c.js";import"./IconsList.module-e4fe8555.js";import"./IconPanTool24-f1398bf1.js";import"./index-c1da5c13.js";const c="Accordion",p="https://www.figma.com/file/MSMqfqJrQNaqbLe4Ctkq7n/Design-System-2.0-NLMK-(beta)-(Community)?type=design&node-id=1026%3A7066&mode=design&t=awyt3Fzj1XS6th7v-1",T=()=>{const[i,o]=b.useState(0),m=A=>i===A;return n("div",{className:a.wrapper,children:[t(v,{title:c,description:`Компонент "аккордеон" предоставляет большие объемы контента в ограниченном пространстве с помощью пошагового раскрытия. Заголовок обеспечивает пользователю общий обзор содержимого, позволяя решить, какие разделы читать.
+
+        Аккордеоны могут сделать обработку информации и поиск более эффективными. Однако они скрывают контент от пользователей, и важно учесть, что пользователь может не заметить или не прочитать весь включенный контент. Если пользователь, вероятно, должен прочесть весь контент, не используйте аккордеон, так как это добавляет дополнительный клик; вместо этого используйте полноценную прокручиваемую страницу с обычными заголовками.`,isStable:!0,codeLink:`https://github.com/nlmk-group/ds-2.0/tree/main/src/components/${c}`,figmaLink:p}),t("div",{className:a.tabs,children:n(r,{children:[t(r.Tab,{label:"Разработчику",active:m(0),onClick:()=>o(0)}),t(r.Tab,{label:"Дизайнеру",active:m(1),onClick:()=>o(1)}),t(r.Tab,{label:"Тестирование",active:m(2),onClick:()=>o(2)})]})}),i==0&&n(u,{children:[t(e,{description:"Аккордеон по умолчанию",code:`
+import { Accordion } from '@nlmk/ds-2.0';
+
+const items = [
+    {
+      id: 1,
+      title: 'First accordion item',
+      content: 'Lorem ipsum',
+      initiallyExpanded: true
+    },
+    {
+      id: 2,
+      title: '2nd accordion item',
+      content: 'Lorem ipsum'
+    },
+    {
+      id: 3,
+      title: 'Last accordion item',
+      content: 'Lorem ipsum'
+    }
+  ]
+
+export default App = () => (
+  <Accordion items={items} />
+)
+              `}),t(e,{description:`Варианты размеров аккордеона: ${Object.values(x).join(" | ")}`,code:`
+import { Accordion } from '@nlmk/ds-2.0';
+
+const items = [
+  {
+    id: 1,
+    title: 'First accordion item',
+    content: 'Lorem ipsum',
+    initiallyExpanded: true
+  },
+  {
+    id: 2,
+    title: '2nd accordion item',
+    content: 'Lorem ipsum'
+  },
+  {
+    id: 3,
+    title: 'Last accordion item',
+    content: 'Lorem ipsum'
+  }
+]
+
+export default App = () => (
+  <>
+  <Accordion size="m" items={items} />
+  <Accordion size="s" items={items} />
+  <Accordion size="xs" items={items} />
+  </>
+)
+              `}),t(e,{description:`Варианты иконок аккордеона: ${Object.values(y).join(" | ")}`,code:`
+import { Accordion } from '@nlmk/ds-2.0';
+
+const items = [
+  {
+    id: 1,
+    title: 'First accordion item',
+    content: 'Lorem ipsum',
+    initiallyExpanded: true
+  },
+  {
+    id: 2,
+    title: '2nd accordion item',
+    content: 'Lorem ipsum'
+  },
+  {
+    id: 3,
+    title: 'Last accordion item',
+    content: 'Lorem ipsum'
+  }
+]
+
+export default App = () => (
+  <>
+  <Accordion startIcon="arrow" items={items} />
+  <Accordion startIcon="plus" items={items} />
+  <Accordion startIcon={null} endIcon="arrow" items={items} />
+  <Accordion startIcon={null} endIcon="plus" items={items} />
+  </>
+)
+              `}),t(e,{description:`Цветовые варианты аккордеона: ${Object.values(L).join(" | ")}`,code:`
+import { Accordion } from '@nlmk/ds-2.0';
+
+const items = [
+  {
+    id: 1,
+    title: 'First accordion item',
+    content: 'Lorem ipsum',
+    initiallyExpanded: true
+  },
+  {
+    id: 2,
+    title: '2nd accordion item',
+    content: 'Lorem ipsum'
+  },
+  {
+    id: 3,
+    title: 'Last accordion item',
+    content: 'Lorem ipsum'
+  }
+]
+
+export default App = () => (
+  <>
+  <Accordion variant="default" items={items} />
+  <Accordion variant="paper" items={items} />
+  <Accordion variant="default" items={items} disabled />
+  <Accordion variant="paper" items={items} disabled />
+  </>
+)
+              `}),t(w,{argsTypes:C})]}),i==1&&t(j,{url:p}),i==2&&t(M,{variant:"Heading4",color:"primary",children:t(h,{componentName:c})})]})};function d(i){return n(u,{children:[t(f,{title:"Components/Accordion/Info",component:l,parameters:{design:{type:"figma",url:"https://www.figma.com/file/cApCjzPxrt7tpXhNjyxSlR/DS2.0-Accordeon?node-id=1110%3A23815&mode=dev"},jest:["Accordion.test.tsx"]}}),`
+`,t(g,{children:t(T,{})})]})}function k(i={}){const{wrapper:o}=Object.assign({},E(),i.components);return o?t(o,{...i,children:t(d,{...i})}):d()}const S=()=>{throw new Error("Docs-only story")};S.parameters={docsOnly:!0};const s={title:"Components/Accordion/Info",parameters:{design:{type:"figma",url:"https://www.figma.com/file/cApCjzPxrt7tpXhNjyxSlR/DS2.0-Accordeon?node-id=1110%3A23815&mode=dev"},jest:["Accordion.test.tsx"]},component:l,tags:["stories-mdx"],includeStories:["__page"]};s.parameters=s.parameters||{};s.parameters.docs={...s.parameters.docs||{},page:k};const xt=["__page"];export{xt as __namedExportsOrder,S as __page,s as default};
---
+++ assets/InputSlider.stories-76bfd96f.js
@@ -0,0 +1,53 @@
+import{M as c,U as g}from"./chunk-HLWAVYOI-653a1315.js";import{I as m}from"./index-2cdebf62.js";import{a as o,j as e,F as l}from"./clsx.m-3764cf42.js";import{r as f}from"./index-f1f749bf.js";import{s as a,H as h,E as p,T as b}from"./Tests-27e47391.js";import{F as S}from"./FigmaEmbed-3478e7e6.js";import{P as y}from"./Properties-72516426.js";import{a as i}from"./index-28aa8d3c.js";import"./index-a4c35820.js";import"./index-63601013.js";import"./index-e99519f1.js";import"./index-cd305b84.js";import"./index-147655d7.js";import{T as I}from"./index-fb030d50.js";import"./index-ed56af28.js";import"./index-5196d16d.js";import"./index-28363b14.js";import"./index-8c6efd17.js";import{u as v}from"./index-4fb8b842.js";import"./iframe-697674bb.js";import"../sb-preview/runtime.js";import"./react-16-8c769108.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-f8a8f14b.js";import"./get-78cac09e.js";import"./callBound-57fbc82c.js";import"./mapValues-f98d729d.js";import"./index-b75c9059.js";import"./uniq-b8c8c302.js";import"./index-356e4a49.js";import"./index-c4c1b386.js";import"./index-29c055d1.js";import"./index-c1da5c13.js";import"./.jest-test-results-7c3b4ed9.js";import"./IconArticle24-414bfa4c.js";import"./IconsList.module-e4fe8555.js";import"./IconPanTool24-f1398bf1.js";const C={onChange:{description:"Callback, который будет вызван при изменении значения внутри компонента InputSlider",action:"изменено value"},value:{description:"Значения компонента InputSlider"},step:{description:"Шаг изменения значения в компоненте InputSlider",table:{defaultValue:{summary:1},type:{summary:"number"}},control:{type:"number"}},min:{description:"Минимальное значение компонента InputSlider",control:{type:"number"}},max:{description:"Максимальное значение компонента InputSlider",control:{type:"number"}},disabled:{description:"Условие блокировки input для ввода/изменений",table:{defaultValue:{summary:"false"},type:{summary:"boolean"}},control:{type:"boolean"}}},w=()=>{const[t,r]=f.useState(0),d=`import { useState } from 'react';
+import { InputSlider, Chip } from '@nlmk/ds-2.0';
+
+const App = () => {
+    const [value, setValue] = useState(20);
+
+    const handleChange = (newValue) => {
+      setValue(newValue);
+    };
+
+    return (
+        <div style={{width: '400px', marginTop: '50px'}}>
+            <InputSlider
+                min={0}
+                max={100}
+                step={1}
+                value={value}
+                onChange={handleChange}
+            />
+            <Chip>{value}</Chip>
+        </div>
+    );
+  };
+
+export default App;
+`,u=`import { useState } from 'react';
+
+import { InputSlider } from '@nlmk/ds-2.0';
+
+const App = () => {
+    const [value, setValue] = useState(500);
+
+    const handleChange = (newValue) => {
+      setValue(newValue);
+    };
+
+    return (
+        <div style={{width: '400px', marginTop: '50px'}}>
+            <InputSlider
+                min={100}
+                max={1000}
+                step={10}
+                value={value}
+                onChange={handleChange}
+                disabled
+            />
+        </div>
+    );
+  };
+
+export default App;
+`;return o("div",{className:a.wrapper,children:[e(h,{title:"InputSlider",description:"InputSlider представляет собой слайдер для ввода числовых значений, который позволяет пользователю выбирать значение в заданном диапазоне.",isStable:!0,codeLink:"https://github.com/nlmk-group/ds-2.0/tree/main/src/components/InputSlider",figmaLink:"https://www.figma.com/file/MSMqfqJrQNaqbLe4Ctkq7n/Design-System-2.0-NLMK-(beta)-(Community)?type=design&node-id=1036%3A20852&mode=design&t=awyt3Fzj1XS6th7v-1"}),e("div",{className:a.tabs,children:o(i,{children:[e(i.Tab,{label:"Разработчику",active:Number(t)===0,onClick:()=>r(0)}),e(i.Tab,{label:"Дизайнеру",active:Number(t)===1,onClick:()=>r(1)}),e(i.Tab,{label:"Тестирование",active:Number(t)===2,onClick:()=>r(2)})]})}),Number(t)===0&&o(l,{children:[e(p,{height:350,description:"InputSlider по умолчанию. Позволяет пользователю выбирать значение от 0 до 100.",code:d}),e(p,{height:350,description:"Неактивный InputSlider. Слайдер установлен в состояние 'disabled', что предотвращает взаимодействие пользователя.",code:u}),e(y,{argsTypes:C})]}),Number(t)===1&&e(S,{url:"https://www.figma.com/file/MSMqfqJrQNaqbLe4Ctkq7n/Design-System-2.0-NLMK-(beta)-(Community)?type=design&node-id=1036%3A20852&mode=design&t=awyt3Fzj1XS6th7v-1"}),Number(t)===2&&e(I,{variant:"Heading4",color:"primary",children:e(b,{componentName:"InputSlider"})})]})};function s(t){return o(l,{children:[e(c,{title:"Components/InputSlider/Info",component:m,parameters:{design:{type:"figma",url:"https://www.figma.com/file/3dTe0kYNFqQO3VnjJ10jyv/DS2.0-Input?type=design&node-id=944-6995&t=xN2Xxpz0c5cAQi98-0"},jest:["InputSlider.test.tsx"]}}),`
+`,e(g,{children:e(w,{})})]})}function x(t={}){const{wrapper:r}=Object.assign({},v(),t.components);return r?e(r,{...t,children:e(s,{...t})}):s()}const N=()=>{throw new Error("Docs-only story")};N.parameters={docsOnly:!0};const n={title:"Components/InputSlider/Info",parameters:{design:{type:"figma",url:"https://www.figma.com/file/3dTe0kYNFqQO3VnjJ10jyv/DS2.0-Input?type=design&node-id=944-6995&t=xN2Xxpz0c5cAQi98-0"},jest:["InputSlider.test.tsx"]},component:m,tags:["stories-mdx"],includeStories:["__page"]};n.parameters=n.parameters||{};n.parameters.docs={...n.parameters.docs||{},page:x};const le=["__page"];export{le as __namedExportsOrder,N as __page,n as default};

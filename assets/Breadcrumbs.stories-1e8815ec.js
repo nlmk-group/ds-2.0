26f2788f42b4660f6836dc8c12b1996c3f9118f3
nlmk-group <120450602+nlmk-group@users.noreply.github.com>
2024-01-19 13:54:24 +0000

    Deploying to gh-pages from  @ b756e40ea17eb58a6134261ad6f3c3aa81ab5e31 🚀

---
+++ assets/Breadcrumbs.stories-1e8815ec.js
@@ -0,0 +1,24 @@
+import{M as u,U as l}from"./chunk-HLWAVYOI-27e3fbbe.js";import{a as m,e as d}from"./index-28aa8d3c.js";import{a as o,j as r,F as c}from"./clsx.m-3764cf42.js";import{r as b}from"./index-f1f749bf.js";import{s as a,H as f,E as i,T as g}from"./Tests-599c93e7.js";import{F as y}from"./FigmaEmbed-c3e8c5ae.js";import{P as h}from"./Properties-94ccce17.js";/* empty css               */import"./index-a4c35820.js";import"./index-63601013.js";import"./index-e99519f1.js";import"./index-cd305b84.js";import"./index-147655d7.js";import{T as w}from"./index-fb030d50.js";import"./index-ed56af28.js";import"./index-5196d16d.js";import"./index-2cdebf62.js";import"./index-28363b14.js";import"./index-8c6efd17.js";import{b as B,a as p,c as N}from"./constants-39cf7234.js";import{a as S}from"./argsTypes-2a5e9fc3.js";import{u as C}from"./index-4fb8b842.js";import"./iframe-69cd47d6.js";import"../sb-preview/runtime.js";import"./react-16-8c769108.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-f8a8f14b.js";import"./get-78cac09e.js";import"./callBound-57fbc82c.js";import"./mapValues-f98d729d.js";import"./index-b75c9059.js";import"./uniq-b8c8c302.js";import"./index-356e4a49.js";import"./index-85cbf9dc.js";import"./index-29c055d1.js";import"./IconArticle24-414bfa4c.js";import"./IconsList.module-e4fe8555.js";import"./IconPanTool24-f1398bf1.js";import"./.jest-test-results-1402245a.js";import"./index-c1da5c13.js";const x=()=>{const[e,t]=b.useState(0);return o("div",{className:a.wrapper,children:[r(f,{title:"Breadcrumbs",description:"Компонент BreadCrumbs (хлебные крошки) используется в пользовательском интерфейсе для предоставления визуальной навигационной структуры, позволяющей пользователям быстро и эффективно понимать своё местоположение в иерархии контента или приложения. BreadCrumbs представляют собой последовательность ссылок или текста, а каждый элемент в ней указывает на уровень иерархии, переходя от более общего к более специфическому контенту.",isStable:!0,codeLink:"https://github.com/nlmk-group/ds-2.0/tree/main/src/components/Breadcrumbs",figmaLink:"https://www.figma.com/file/MSMqfqJrQNaqbLe4Ctkq7n/Design-System-2.0-NLMK-(beta)-(Community)?type=design&node-id=1036%3A20843&mode=design&t=awyt3Fzj1XS6th7v-1"}),r("div",{className:a.tabs,children:o(m,{children:[r(m.Tab,{label:"Разработчику",active:Number(e)===0,onClick:()=>t(0)}),r(m.Tab,{label:"Дизайнеру",active:Number(e)===1,onClick:()=>t(1)}),r(m.Tab,{label:"Тестирование",active:Number(e)===2,onClick:()=>t(2)})]})}),Number(e)==0&&o(c,{children:[r(i,{description:"Обычное использование компонента.",code:`import { Breadcrumbs } from '@nlmk/ds-2.0';
+
+export default  App = () => (
+  <>
+    <Breadcrumbs crumbs={${JSON.stringify(B,null,4)}} />
+  </>
+)
+            `}),r(i,{description:"Компоненту можно задать значение ширины в процентах. Процент будет считываться отширины родителя.",code:`import { Breadcrumbs } from '@nlmk/ds-2.0';
+
+export default  App = () => (
+  <>
+    <Breadcrumbs crumbs={${JSON.stringify(p,null,4)}} />
+    <Breadcrumbs width="50" crumbs={${JSON.stringify(p,null,4)}} />
+  </>
+)
+            `}),r(i,{description:"Компонент может содержать ссылки с разными target.",code:`import { Breadcrumbs } from '@nlmk/ds-2.0';
+
+export default  App = () => (
+  <>
+    <Breadcrumbs crumbs={${JSON.stringify(N,null,4)}} />
+  </>
+)
+            `}),r(h,{argsTypes:S})]}),Number(e)==1&&r(y,{url:"https://www.figma.com/file/MSMqfqJrQNaqbLe4Ctkq7n/Design-System-2.0-NLMK-(beta)-(Community)?type=design&node-id=1036%3A20843&mode=design&t=awyt3Fzj1XS6th7v-1"}),Number(e)==2&&r(w,{variant:"Heading4",color:"primary",children:r(g,{componentName:"Breadcrumbs"})})]})};function n(e){return o(c,{children:[r(u,{title:"Components/Breadcrumbs/Info",component:d,parameters:{design:{type:"figma",url:"https://www.figma.com/file/CTuqaCtvStjoGNh3wL61zx/DS2.0-Header?type=design&node-id=510-15378&mode=design&t=BH844rIxMaUmKuVe-0"},jest:["Breadcrumbs.test.tsx"]}}),`
+`,r(l,{children:r(x,{})})]})}function M(e={}){const{wrapper:t}=Object.assign({},C(),e.components);return t?r(t,{...e,children:r(n,{...e})}):n()}const T=()=>{throw new Error("Docs-only story")};T.parameters={docsOnly:!0};const s={title:"Components/Breadcrumbs/Info",parameters:{design:{type:"figma",url:"https://www.figma.com/file/CTuqaCtvStjoGNh3wL61zx/DS2.0-Header?type=design&node-id=510-15378&mode=design&t=BH844rIxMaUmKuVe-0"},jest:["Breadcrumbs.test.tsx"]},component:d,tags:["stories-mdx"],includeStories:["__page"]};s.parameters=s.parameters||{};s.parameters.docs={...s.parameters.docs||{},page:M};const br=["__page"];export{br as __namedExportsOrder,T as __page,s as default};

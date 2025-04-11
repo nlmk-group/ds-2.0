1f056bb61bd987a7ac568b2d9ed8ef7f520f166c
Moonf1ree <shurnd32281@gmail.com>
2025-04-11 15:27:56 +0300

    fix(New-Icon): fixes by review

--- src/components/Icon/IconsDirectory/MES/ClaimScan/16.tsx
+++ src/components/Icon/IconsDirectory/MES/ClaimScan/16.tsx
@@ -12,17 +12,17 @@ const IconClaimScan16: FC<IIconsProps> = ({ color = 'inherit', htmlColor }) => {
   return (
     <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" className={classes}>
       <path
-        fill={htmlColor || '#1952B6'}
+        fill={htmlColor || 'currentColor'}
         fillRule="evenodd"
         clipRule="evenodd"
         d="M8.925 11.681c0-1.283 1.023-2.325 2.285-2.325 1.265 0 2.29 1.042 2.29 2.325 0 1.284-1.025 2.325-2.29 2.325-1.262 0-2.285-1.041-2.285-2.325Zm2.059-1.162h.457v1.395h-.457v-1.396Zm0 1.86h.457v.465h-.457v-.465Z"
       />
       <path
-        fill={htmlColor || '#1952B6'}
+        fill={htmlColor || 'currentColor'}
         d="M4.198 5.265c.652-1.322 1.68-2.31 2.82-3.003l-.392-.647c-1.777 1.08-3.499 2.956-3.841 5.807a5.253 5.253 0 0 0 6.635 6.693 3.04 3.04 0 0 1-.619-.61 4.496 4.496 0 1 1 3.438-4.668c.276.1.533.239.765.41l.002-.115a5.253 5.253 0 0 0-8.808-3.867Z"
       />
       <path
-        fill={htmlColor || '#1952B6'}
+        fill={htmlColor || 'currentColor'}
         d="M11.43 8.666a3.708 3.708 0 1 0-3.033 4.117 3.005 3.005 0 0 1-.185-.737 2.95 2.95 0 1 1 2.46-3.34 3.04 3.04 0 0 1 .759-.04Z"
       />
     </svg>
--- src/components/Icon/IconsDirectory/MES/ClaimScan/24.tsx
+++ src/components/Icon/IconsDirectory/MES/ClaimScan/24.tsx
@@ -12,17 +12,17 @@ const IconClaimScan24: FC<IIconsProps> = ({ color = 'inherit', htmlColor }) => {
   return (
     <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" className={classes}>
       <path
-        fill={htmlColor || '#1952B6'}
+        fill={htmlColor || 'currentColor'}
         fillRule="evenodd"
         clipRule="evenodd"
         d="M13.388 17.522c0-1.925 1.533-3.488 3.427-3.488 1.898 0 3.435 1.563 3.435 3.488s-1.537 3.487-3.435 3.487c-1.894 0-3.427-1.562-3.427-3.487Zm3.088-1.744h.686v2.092h-.686v-2.092Zm0 2.79h.686v.697h-.686v-.697Z"
       />
       <path
-        fill={htmlColor || '#1952B6'}
+        fill={htmlColor || 'currentColor'}
         d="M6.297 7.897c.979-1.983 2.52-3.465 4.231-4.503l-.59-.971c-2.665 1.618-5.247 4.434-5.761 8.71a7.88 7.88 0 0 0 9.952 10.04 4.563 4.563 0 0 1-.927-.916 6.744 6.744 0 1 1 5.156-7.001c.415.15.8.357 1.149.614l.002-.172A7.88 7.88 0 0 0 6.297 7.897Z"
       />
       <path
-        fill={htmlColor || '#1952B6'}
+        fill={htmlColor || 'currentColor'}
         d="M17.146 13a5.561 5.561 0 1 0-4.55 6.175 4.503 4.503 0 0 1-.279-1.106 4.425 4.425 0 1 1 3.691-5.01 4.558 4.558 0 0 1 1.138-.06Z"
       />
     </svg>

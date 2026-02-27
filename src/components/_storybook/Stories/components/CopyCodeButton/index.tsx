8ef27d0490d0e22c8ee7ea6a28bfad8afb53f067
Vadim Kirpun <kirpun_vv@nlmk.com>
2026-02-27 10:03:53 +0300

    fix(DESIGNSYS-2353): fix CopyCodeButton functionality
    Removed line numbering from the copied code in the CopyCodeButton component to streamline the copy process and enhance usability.

--- src/components/_storybook/Stories/components/CopyCodeButton/index.tsx
+++ src/components/_storybook/Stories/components/CopyCodeButton/index.tsx
@@ -6,17 +6,14 @@ import React from 'react';
 const CopyCodeButton: FC<{ code: string }> = ({ code }) => {
   const [isCopied, setIsCopied] = useState(false);
 
-  const lines = code.split('\n');
-  const codeWithNumbers = lines.map((line, i) => `${i + 1} ${line}`).join('\n');
-
   const copyToClipboard = useCallback(() => {
-    copyUtils(codeWithNumbers, () => {
+    copyUtils(code, () => {
       setIsCopied(true);
       setTimeout(() => setIsCopied(false), 2000);
     });
-  
+
   }, [code]);
-  
+
   return (
     <Tooltip render={<Typography variant="Body2-Bold">{isCopied ? 'Скопировано' : 'Копировать'}</Typography>}>
       <Button

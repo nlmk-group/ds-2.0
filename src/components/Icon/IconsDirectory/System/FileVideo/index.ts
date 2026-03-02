a8e0670ee32a36e1228f87a5212782a939599f4b
borisova_av <borisova_av@nlmk.com>
2026-03-02 14:41:57 +0300

    feat(CLAIM-3578): generate icons

---
+++ src/components/Icon/IconsDirectory/System/FileVideo/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Этот файл сгенерирован автоматически.
+ */
+
+export { default as IconFileVideo16 } from './16';
+export { default as IconFileVideo24 } from './24';
+export { default as IconFileVideo32 } from './32';

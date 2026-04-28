3e62f3883b6c1cba438cb98777a0ad36cf49776d
Naughty1905 <minion1905@gmail.com>
2026-04-28 14:14:01 +0300

    fix(DESIGNSYS-2386): update styles for improved AttachFiles

--- src/components/AttachFiles/index.tsx
+++ src/components/AttachFiles/index.tsx
@@ -26,11 +26,7 @@ const AttachFiles: FC<IAttachFilesProps> = ({ title, children, className }) => {
       <div data-ui-attach-files-title data-testid="ATTACHFILES_TITLE">
         {title}
       </div>
-      <div
-        data-ui-attach-files-list
-        className={clsx(title && styles['list-with-title'])}
-        data-testid="ATTACHFILES_LIST"
-      >
+      <div data-ui-attach-files-list data-testid="ATTACHFILES_LIST">
         {children}
       </div>
     </div>

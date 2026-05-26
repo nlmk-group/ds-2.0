69753e960488d14b3544a224c09da5a0fe2cf6b3
samarkin_ms <samarkin_ms@nlmk.com>
2026-05-26 17:03:17 +0500

    fix(DESIGNSYS-2470): fix select - 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { SelectExm } from './components/Select/example';
+import { SelectExm } from './components/Select/examples';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);

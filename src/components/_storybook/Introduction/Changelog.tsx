e4997bf7efa9efada70f98d27541bc0811865806
Naughty1905 <minion1905@gmail.com>
2026-08-18 15:59:14 +0300

    docs(DESIGNSYS-2538): update 2.32.0 release date

--- src/components/_storybook/Introduction/Changelog.tsx
+++ src/components/_storybook/Introduction/Changelog.tsx
@@ -23,7 +23,7 @@ export const Changelog = () => {
         </div>
         <div className={styles.history}>
           <Typography variant="Subheading3-Medium">
-            v2.32.0 - <span className={styles.date}>30.07.26</span>
+            v2.32.0 - <span className={styles.date}>18.08.26</span>
           </Typography>
           <ul className={styles.list}>
             <li>
